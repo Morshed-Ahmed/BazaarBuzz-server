ba33e65c8ebd3b198bc5725166932a7422f56eb9
morshedAhmed <morshed50r@gmail.com>
2024-02-27 16:23:33 +0600

    product filter addad

--- products/views.py
+++ products/views.py
@@ -7,14 +7,21 @@ class TopSubCategoryFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         top_category = request.query_params.get('top_category')
         sub_category = request.query_params.get('sub_category')
+        search_query = request.query_params.get('search')
 
         if top_category:
             queryset = queryset.filter(top_category__name=top_category)
         if sub_category:
             queryset = queryset.filter(sub_category__name=sub_category)
+        
+        if search_query == "All":
+            return queryset
+        
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
 
         return queryset
-
+        
 class ProductListCreate(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
